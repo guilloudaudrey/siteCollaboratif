a283e5a5fd76552eea3d9c774994c75d476a25ac
Audrey Guilloud <audrey.guilloud2201@gmail.com>
2017-09-17 12:30:18 +0200

    espace perso boot

--- espaceperso.php
+++ espaceperso.php
@@ -2,18 +2,18 @@
 
 <html>
     <head>
-    <meta charset="UTF-8">
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">      
-    <meta name="viewport" content="width=device-width, user-scalable=yes" /><!--user-scalable=yes” sert à indiquer que l’utilisateur peut zoomer sur le contenu-->
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
-    <script src="js/modernizr.custom.js"></script>
-    <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
+        <meta charset="UTF-8">
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">      
+        <meta name="viewport" content="width=device-width, user-scalable=yes" /><!--user-scalable=yes” sert à indiquer que l’utilisateur peut zoomer sur le contenu-->
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+        <script src="js/modernizr.custom.js"></script>
+        <script src="http://code.jquery.com/jquery-1.12.1.min.js"></script>
     </head>
     <body>
         <?php
@@ -23,87 +23,83 @@
         include_once 'classes/Post.php';
         include_once 'classes/Comment.php';
         $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
         $instance = new DataBase;
-
-        include_once 'html/header.php';
-
-
         if (isset($_SESSION['nom'])) {
             $user = $_SESSION ['nom'];
 
             $contenu = $instance->readUser($user);
 
-            echo '<div class="container" style=" box-shadow: 0px 0px 10px 0px rgba(119, 119, 119, 0.67);
-            -moz-box-shadow: 0px 0px 10px 0px rgba(119, 119, 119, 0.67);
-            -webkit-box-shadow: 0px 0px 10px 0px rgba(119, 119, 119, 0.67);
-            background-color:white; margin-top: 100px; padding:80px">'.$contenu->asHtml();
-            //echo '<form action="logout.php" method="POST"><button>Se déconnecter</button></form>';
-            echo '<a href="post_form.php">Créer une nouvelle annonce</a><br/>';
-            echo '<a href="index.php">Index</a>';
-        } else {
-           // include_once 'html/connexion-html.php';
-        }
-        ?>
+//-----------------------------header------------------------------//
 
-        <h2>Mes annonces</h2>
+            include_once 'html/header.php';
+            ?>
+            <div class="container espacepersocont ">
+                <div class="row">
+                    <div class= "col-lg-5">
+                        <?php
+                        echo $contenu->asHtml();
+                    } else {
+                        // include_once 'html/connexion-html.php';
+                    }
+                    ?>
+                </div>
+                <div class="container col-lg-6">
+                    <h2>Mes annonces</h2>
 
-        <?php
-        include_once 'classes/Post.php';
-        include_once 'classes/DataBase.php';
-        include_once 'classes/User.php';
+                    <?php
+                    $listeAnnonces = $instance->readPostsList();
+                    foreach ($listeAnnonces as $annonce) {
+                        $author = $annonce->getAuthor();
 
-        $listeAnnonces = $instance->readPostsList();
-        foreach ($listeAnnonces as $annonce) {
-            $author = $annonce->getAuthor();
+                        if ($author == $user) {
+                            ?>
+                            <div class="container col-lg-12"><?php echo $annonce->asHtml() ?>
+                                <div class="boutons">
+                                    <form method="POST" action="delete.php">
+                                        <input type="hidden" name="filename" value="<?php echo $annonce->getTitle() ?>" class="text">
+                                        <input type="submit" value="supprimer">
+                                    </form>
 
-            if ($author == $user) {
-                echo '<div class="container">'. $annonce->asHtml();
+                                    <form method="POST" action="edit_form.php">
+                                        <input type="hidden" name="filename" value="<?php $annonce->getTitle() ?>">
+                                        <input type="submit" value="modifier">
+                                    </form>
+                                </div></div>
+                            <?php
+                        }
+                    }
+                    ?>
+                    <h2>Evaluations émises</h2>
+                    <?php
+                    $listecomm = $instance->readCommentsList();
+                    $listepost = $instance->readPostsList();
 
-                echo'
+                    foreach ($listecomm as $comm) {
+                        if ($user == $comm->getAuthor()) {
 
-            <div class="boutons">
-            <form method="POST" action="delete.php">
-            <input type="hidden" name="filename" value="' . $annonce->getTitle() . '" class="text">
-            <input type="submit" value="supprimer">
-            </form>
-    
-            <form method="POST" action="edit_form.php">
-            <input type="hidden" name="filename" value="' . $annonce->getTitle() . '">
-                <input type="submit" value="modifier">
-            </form>
-            </div></div>';
-            }
-        }
-        ?>
-        <h2>Evaluations émises</h2>
-        <?php
-        $listecomm = $instance->readCommentsList();
-        $listepost = $instance->readPostsList();
+                            echo $comm->asHtml();
+                        }
+                    }
+                    ?>
+                    <h2>Evaluations reçues</h2>
+                    <?php
+                    foreach ($listecomm as $comm) {
 
-        foreach ($listecomm as $comm) {
-            if ($user == $comm->getAuthor()) {
+                        if ($user == $author) {
 
-                echo $comm->asHtml();
-            }
-        }
-        ?>
-        <h2>Evaluations reçues</h2>
-        <?php
-     
-            foreach ($listecomm as $comm) {
-           
-                if ($user == $author) {
-                 
-             
-                    echo $comm->asHtml();
-                }
-            }
-        
-        ?>
+
+                            echo $comm->asHtml();
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
+
         <?php
-include_once 'html/footer.php';
-?>
+        include_once 'html/footer.php';
+        ?>
+
+
     </body>
 </html>
--- html/header.php
+++ html/header.php
@@ -1,56 +1,60 @@
 
-            <nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
-                <div class="container topnav ">
-                    <div class="navbar-header col-md-9 col-lg-6" >
-                        <a class="navbar-brand " href="index.php">HLP</a>
-                    </div>
-                    <?php
-                    $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                    if (!isset($_SESSION['nom'])) {
-                        ?>
-                        <ul class="nav navbar-nav col-lg-6" >
-                            <li class="pull-right"><a href="register-form.php">S'inscrire</a></li>
-                            <li class="pull-right"><a href="#?w=500" rel="popup_name" data-toggle="modal" data-target="#myModal">Se connecter</a></li>
-                        </ul>
-                        <?php
-                    } else {
-                        echo '<div class="col-lg-5 col-md-7 pull-right"><div class="col-lg-4" style="margin-top: 14px"> Bonjour ' . $_SESSION['nom'] .' !';
-                        echo '</div><form action="logout.php" method="POST" class="col-lg-4 pull-right" style="margin-top: 8px; text-align: center"><button class="btn btn-default ">Se déconnecter</button></form>';
-                        echo '<div class="col-lg-4 pull-right" style="margin-top: 8px"><a class = "btn btn-default" href="espaceperso.php" role="button">Espace personnel</a></div></div>';
-                        ;
-                    }
-                    ?>
-                </div>
-            </nav>
+<nav class="navbar navbar-default navbar-fixed-top topnav .navbar-expand{-sm}" role="navigation">
+    <div class="container topnav ">
+        <div class="navbar-header col-md-9 col-lg-6" >
+            <a class="navbar-brand " href="index.php">HLP</a>
+        </div>
+        <?php
+        $url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        if (!isset($_SESSION['nom'])) {
+            ?>
+            <ul class="nav navbar-nav col-lg-6" >
+                <li class="pull-right"><a href="register-form.php">S'inscrire</a></li>
+                <li class="pull-right"><a href="#?w=500" rel="popup_name" data-toggle="modal" data-target="#myModal">Se connecter</a></li>
+            </ul>
+            <?php
+        } else {
+            ?>
+            <div class="col-lg-5 col-md-7 pull-right"><div class="col-lg-4" style="margin-top: 14px"> Bonjour <?php echo $_SESSION['nom'] ?> !</div>
+                <form action="logout.php" method="POST" class="col-lg-4 pull-right" style="margin-top: 8px; text-align: center">
+                    <button class="btn btn-default ">Se déconnecter</button>
+                </form>
+                <div class="col-lg-4 pull-right" style="margin-top: 8px"><a class = "btn btn-default" href="espaceperso.php" role="button">Espace personnel</a></div>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</nav>
 
 
-    <!-------------------------------------fenêtre pop up connexion----------------------------------->
+<!-------------------------------------fenêtre pop up connexion----------------------------------->
 
-    <div id="myModal" class="modal fade" role="dialog">
-        <div class="modal-dialog">
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">
 
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Connexion</h4>
-                </div>
-                <div class="modal-body">
-                    <form method="POST" action="login.php">
-                        <label for="pseudo">Pseudo</label>
-                        <input type="text" name="pseudo"/>
-                        <label for="mdp">Mot de passe</label>
-                        <input type="password" name="mdp"/>
-                        <input type="submit" name="login"/>
-                        <input type="hidden" name="url" value="<?php echo $url; ?>"/>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
-                </div>
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Connexion</h4>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="login.php">
+                    <label for="pseudo">Pseudo</label>
+                    <input type="text" name="pseudo"/>
+                    <label for="mdp">Mot de passe</label>
+                    <input type="password" name="mdp"/>
+                    <input type="submit" name="login"/>
+                    <input type="hidden" name="url" value="<?php echo $url; ?>"/>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Fermer</button>
             </div>
         </div>
     </div>
+</div>
 
 
- 
+
